cc70cb9d8d3dd095b98da8334dc92023e13b4b7e
elbromed <elbromed@gmail.com>
2020-03-09 20:36:32 -0400

    https://github.com/elbromed/arm.git

--- deployAzFw.ps1
+++ deployAzFw.ps1
@@ -2,11 +2,11 @@
 $Location="canadacentral"
 $rsg="RG-HUB"
 
-$parameterFile="C:\studio\repo\azfw06.parameters.json"
-$templateFile = "C:\studio\repo\azfw06.template.json"
+$parameterFile="C:\studio\repo\azfw07.parameters.json"
+$templateFile = "C:\studio\repo\azfw07.template.json"
 $Namedeployment="AzFwEnvironment"
 
 New-AzResourceGroup -Name $rsg -Location $Location 
 New-AzResourceGroupDeployment -Name $Namedeployment -ResourceGroupName $rsg -TemplateFile $templateFile -TemplateParameterFile $parameterFile -Verbose
 
-#az group deployment validate --resource-group RG-HUB --template-file .\azfw01.template.json --parameters .\azfw01.parameters.json
+#az group deployment validate --resource-group RG-HUB --template-file .\azfw07.template.json --parameters .\azfw07.parameters.json
